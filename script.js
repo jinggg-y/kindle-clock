c9ac1de449c42594eec162cecb87ab1459841ee4
Jing <y.dianna.j@gmail.com>
2022-11-11 22:42:53 +1000

    update time zone

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function Time(){
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-    var d = new Date().toLocaleString("en-AU", {timeZone: "Australia/Brisbane"});
+    var d = new Date();
     var hour = d.getHours();
     var minute = d.getMinutes();
     var day = d.getDay();
@@ -9,12 +9,10 @@ function Time(){
     var month = d.getMonth();
     var year = d.getFullYear();
 
-    hour = update(hour);
-    minute = update(minute);
     day = days[day - 1];
     month += 1;
 
-    document.getElementById("clock").innerText = hour + " : " + minute;
+    document.getElementById("clock").innerText = d.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
     document.getElementById("day").innerText = day;
     document.getElementById("date").innerText = date + "/" + month + "/" + year;
      // Set Timer to 0.5 minute (30000 ms)
